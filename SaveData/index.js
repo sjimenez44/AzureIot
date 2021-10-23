bb4b1ed0c1c7507c7ba8f58b11721e9923f9b8f0
Santiago Jimenez <santiji00@gmail.com>
2021-10-22 20:48:24 -0500

    refactor: add process and save data

--- SaveData/index.js
+++ SaveData/index.js
@@ -1,20 +1,26 @@
-const response = require('../network/response');
+const responsef = require('../network/response');
+const process = require('./processdata');
+const save = require('./savedata');
 
 module.exports = async function (context, req) {
   if (req.body && req.body.type) {
-    if (req.body.value) {
-      const sensor = process.processdata(req.body);
-      const toSend = {
-        num: 1,
-        sensor
-      };
-      context.res = response.success(200, toSend);
+    if (req.body.id) {
+      if (req.body.value) {
+        const sensor = process.processdata(req.body);
+        const result = await save.savedata(sensor);
+        (result)
+          ? context.res = responsef.success(201, null)
+          : context.res = responsef.error(400, null, null);
+      } else {
+        context.res = responsef.error(400, null, 
+          'Please insert the data value');
+      }
     } else {
-      context.res = response.error(400, null, 
-        'Please insert the data value');
+      context.res = responsef.error(400, null, 
+        'Please define the sensor id');
     }
   } else {
-    context.res = response.error(400, null, 
+    context.res = responsef.error(400, null, 
       'Please define the type of the imput');
   }
 }
